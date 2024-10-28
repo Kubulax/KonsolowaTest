79d28981f278841fad965acadee2f66477c38bd3
Jakub_Koza_spr <kubulax25@gmail.com>
2024-10-28 13:16:13 +0100

    Zaktualizowano testy

--- KonsolowaTests.cs
+++ KonsolowaTests.cs
@@ -10,9 +10,25 @@ namespace KonsolowaTests
             //Arrange
             string path = "../../../Data.txt";
             File.WriteAllLines(path, ["Gorillaz", "\"The Now Now\"", "11", "2018", "11000102", "", "My Chemical Romance", "\"The Black Parade\"", "14", "2006", "304666444"]);
+            List<MusicAlbum> expectedAlbums = new List<MusicAlbum>([new MusicAlbum("Gorillaz", "\"The Now Now\"", 11, "2018", 11000102), new MusicAlbum("My Chemical Romance", "\"The Black Parade\"", 14, "2006", 304666444)]);
 
             //Act
-            List<MusicAlbum> albums = Program.Rea
+            List<MusicAlbum> albums = Program.GetMusicAlbums(path);
+
+            //Assert
+            Assert.Equal(2, albums.Count);
+
+            Assert.Equal(expectedAlbums[0].artist, albums[0].artist);
+            Assert.Equal(expectedAlbums[0].album, albums[0].album);
+            Assert.Equal(expectedAlbums[0].songsNumber, albums[0].songsNumber);
+            Assert.Equal(expectedAlbums[0].year, albums[0].year);
+            Assert.Equal(expectedAlbums[0].downloadNumber, albums[0].downloadNumber);
+
+            Assert.Equal(expectedAlbums[1].artist, albums[1].artist);
+            Assert.Equal(expectedAlbums[1].album, albums[1].album);
+            Assert.Equal(expectedAlbums[1].songsNumber, albums[1].songsNumber);
+            Assert.Equal(expectedAlbums[1].year, albums[1].year);
+            Assert.Equal(expectedAlbums[1].downloadNumber, albums[1].downloadNumber);
         }
     }
 }
