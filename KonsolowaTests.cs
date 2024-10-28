9fb195e5fb407c992da34087253fe2e7455a9b2b
Jakub_Koza_spr <kubulax25@gmail.com>
2024-10-28 13:02:26 +0100

    Arrange, assert

--- KonsolowaTests.cs
+++ KonsolowaTests.cs
@@ -1,11 +1,18 @@
-namespace konsolowa
+using konsolowa;
+
+namespace KonsolowaTests
 {
     public class KonsolowaTests
     {
         [Fact]
         public void GivenFilePath_WhenGetMusicAlbums_ReturnListOfAlbums()
         {
+            //Arrange
+            string path = "../../../Data.txt";
+            File.WriteAllLines(path, ["Gorillaz", "\"The Now Now\"", "11", "2018", "11000102", "", "My Chemical Romance", "\"The Black Parade\"", "14", "2006", "304666444"]);
 
+            //Act
+            List<MusicAlbum> albums = Program.Rea
         }
     }
 }
